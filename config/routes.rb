ec984eb5748e543044f9162c35683b77b102c961
Bejenariu Ionut <ibejenariu98@yahoo.com>
2019-05-20 12:52:36 +0300

    FrontEnd

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  #namespace 'api' do
-  #  namespace 'v1' do
-  #    resources :microposts
-  #  end
-  #end
+  namespace 'api' do
+    namespace 'v1' do
+      resources :microposts
+    end
+  end
 
   get 'sessions/new'
 
